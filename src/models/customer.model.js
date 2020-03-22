f825923ef9afed2c43941a3b3b5329a7e3ee876d
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-03-23 01:02:42 +0530

    added port variable
    in the template string port is being referenced

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -1,6 +1,7 @@
 let mongoose = require('mongoose')
 
-const server = 'ds221609.mlab.com:21609'
+const server = 'ds221609.mlab.com'
+const port = '27017'
 const database = 'rest-api-workshop'
 const user = 'theoutlander'
 const password = 'fZsMGZXQMx8FCTgkBwgFtEvwD7ML'
